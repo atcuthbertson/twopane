e9781a1a6045d068a9a0c0cd2f7a88d214958be4
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-09-15 15:33:58 -0700

    Main js file in approximately final state. Work on info module and layers

--- js/twopane.js
+++ js/twopane.js
@@ -116,7 +116,7 @@ function(
     var svgLayer;
     var rp = dom.byId('rightPane');
     var titleNode = dom.byId('titleNode');
-    var layerNode = dom.byId('layerNode');
+    var dataNode = dom.byId('dataNode');
     var closeButton = dom.byId('closeRP');
 
     var closeToggle;
@@ -152,14 +152,6 @@ function(
 	
 
 
-    var infoWindow = new InfoWindow('infoWindow');
-    infoWindow.startup();
-    infoWindow.setTitle('<a id="zoomLink" action="javascript:void 0">Information at this Point</a>')
-
-    var identifyParameters = new IdentifyParameters();
-    identifyParameters.layerOption = IdentifyParameters.LAYER_OPTION_VISIBLE;
-    identifyParameters.tolerance = 3;
-    identifyParameters.returnGeometry = false;
 
 
 
@@ -170,7 +162,6 @@ function(
   	var map = new Map(mapPane, {
         basemap : "topo",
   	    extent:initialExtent,
-        infoWindow:infoWindow,
         minZoom:6,
   	    maxZoom:12
       });
@@ -180,12 +171,6 @@ function(
     window.map = map;
 
 
-
-
-  	var home= new HomeButton({
-  	  map: map
-  	}, "homeButton");
-  	home.startup();
     
 	
 
@@ -200,7 +185,7 @@ function(
       infoWindow.show(0,0);
       setTimeout(function(){infoWindow.hide()},0);
 
-      var basemapToggle = toggle();
+      var basemapToggle = BasemapToggle();
       on(dom.byId("basemapNode"),"mousedown",basemapToggle);
     });
 
@@ -284,9 +269,6 @@ function(
 
 
 
-
-
-
     //could/should likely be a module that each of the layers add
     function addLoading(check){
       var img = DOC.createElement('img')
@@ -329,35 +311,12 @@ function(
 
 
 
+    // Add dijits to the application
 
 
 
-
-
-
-
-
-
-//attach datapane handlers. Called when dijit is loaded.
-
-
-
-//ie shim
-function forEach(arr,fn){
-  for(var i=0;i<arr.length;i++){
-    fn(arr[i],i,arr)
-  }
-}
-
-
-
-
-
-
-// Add dijits to the application
-
-  //custom basemap toggle
-    function toggle(){
+    //custom basemap toggle
+    function BasemapToggle(){
       var t = "topo";
       var s = "satellite";
       var g = "gray";
@@ -394,361 +353,135 @@ function forEach(arr,fn){
       }
     }
 
- //Tabbed InfoWindow with Identify tool 
- 
-  var mdX = 0;
-  var mdY = 0;
-  var lastClick = 0;
-  var wasDouble = 0;
-  var notMap = 0;
-
-  var tabs = new TabContainer({style:"height:100%;"},'infoTabContainer');
-  infoWindow.setContent(tabs.domNode)
-
-infoWindow.on('hide',function(){
-  infoWindow.resize(425,325);
-})
 
 
-  on(mapPane,"mousedown",function(e){
-    mdX = e.clientX;
-    mdY = e.clientY;
-    var now = new Date().getTime();
+    //Home extent button
+    var home= new HomeButton({
+      map: map
+    }, "homeButton");
 
-    if(now - lastClick < 300)
-      wasDouble = 1;
-    else
-      wasDouble = 0;
-
-    lastClick = now;
-
-    //if the first click wasn't on the map either, don't first double
-    if(wasDouble&&notMap){
-      return;
-    }
-
-    if(e.target === svgLayer||e.target.id.slice(0,10) ==="centerPane")
-      notMap = 0;
-    else
-      notMap = 1;
-
-    if(wasDouble)
-      fireZoom(e);
-
-  });
-
-  on(mapPane,"mouseup",function(e){
-    if (notMap) return;
-    if(wasDouble){
-      return wasDouble = 0;
-    }
-    if(Math.abs(e.clientX-mdX)<10&&Math.abs(e.clientY-mdY)<10){
-
-      addEventCoordinates(e);
-      runIdentify(e);
-      setInfoPoint(e);
-    }
-  })
+    home.startup();
 
 
-  function addEventCoordinates(e){
-    var  x = e.clientX - centerPane.offsetLeft -1;
-    var  y = e.clientY - centerPane.offsetTop -1;
+    //Info window and identify
+    var infoWindow = new InfoWindow('infoWindow');
+    infoWindow.startup();
+    infoWindow.setTitle('<a id="zoomLink" action="javascript:void 0">Information at this Point</a>')
+    map.setInfoWindow(infoWindow);
 
-    e.screenPoint = new ScreenPoint(x,y);
-    e.mapPoint = map.toMap(e.screenPoint)
-  }
+    var identifyParameters = new IdentifyParameters();
+    identifyParameters.layerOption = IdentifyParameters.LAYER_OPTION_VISIBLE;
+    identifyParameters.tolerance = 3;
+    identifyParameters.returnGeometry = false;
 
-  function fireZoom(e){
-    addEventCoordinates(e)
-    lastClick = 0;
-    infoWindow.hide();
-    map.centerAndZoom(e.mapPoint,map.getLevel()+1)
-  }
-
-  function setInfoPoint(event){
-    if(infoWindow.zoomHandler)
-      infoWindow.zoomHandler.remove();
-    infoWindow.zoomHandler = on(DOC.getElementById('zoomLink'),'click',function(){
-      map.centerAndZoom(event.mapPoint,12)
-    });
-  }
-
-  function addVisibleUrl(url,service){
-    visibleServiceUrls[url] = service;
-  }
-
-  function removeVisibleUrl(url){
-    visibleServiceUrls[url] = null;
-  }
-
-  function runIdentify(event){
-    var noneShowing = 1;
-    infoWindow.show(event.screenPoint);
-    identifyParameters.geometry = event.mapPoint;
-    identifyParameters.mapExtent = map.extent;
-    identifyParameters.width = map.width;
-    identifyParameters.height = map.height;
-    forEach(tabs.getChildren(),function(v){tabs.removeChild(v)});
-
-    for(var taskUrl in identifyTasks){
-      if(!visibleServiceUrls[taskUrl]) continue;
-        noneShowing = 0;
-        identifyParameters.layerIds = visibleServiceUrls[taskUrl].visibleLayers;
-        identifyTasks[taskUrl].execute(identifyParameters,doProcess(taskUrl))
-    }
-
-    if(noneShowing){
-      setNoData();
-    }
-  }
-
-  function doProcess(url){
-    return function(results){
-      processIdentify(results,url);
-    }
-  }
-
-  function processIdentify (results,url){
-    if(!results.length) return
-    forEach(results,function(result){
-      var tab = new ContentPane(makePane(result,url))
-      tabs.addChild(tab);
-    })
-    tabs.resize();
-  }
-
-  function makePane(result,url){
-    var isChange = !!url.match("GIC_Change");
-    var activeData = !!url.match(/_Change_|_Depth_|_Elevation_/);
-    var title;
-    var blurb;
-    if(activeData){
-      title = getTitle(result,isChange);
-      blurb = getBlurb(title,isChange,url);
-    }else{
-      title = makeSpaced(result.layerName)
-      blurb = '';
-    }
-    var content = makeContent(result.feature.attributes,blurb)
-
-    return{title:title,content:content}
-  }
+    //info.init(map,infoWindow, identifyParameters)
 
 
 
-  function getTitle(result,isChange){
-    var name = result.layerName;
-    var season = name.match(/S|F/);
-    if(season[0] === "S") season = "Spring "
-    else season = "Fall "
 
-    if(isChange){
-      var arr=name.match(/(\d{4}).*(\d{4})/);
-      return arr[2] +" to "+arr[1];
-    }
-    return season+name.match(/\d{4}/)[0]
-  }
+    //toggling right pane
+    closeToggle = function(){
+      var showing = 0;
+      var arro = dom.byId("arro");
+      var movers = query(".mov");
 
-
-
-  function makeContent(attributes,blurb){
-    var list = blurb+"<ul>";
-    for (var key in attributes){
-      if(attributes.hasOwnProperty(key)
-        &&key!=="OBJECTID"
-        &&key!=="Shape"
-        &&key!=="Shape_Area"
-        &&key!=="Shape_Length"
-        &&key!=="Pixel Value"
-        ){
-        var spaced = makeSpaced(key)
-        list+= "<li><strong>"+spaced+"</strong>: "+getAttributeHTML(attributes[key])+"</li>"
+      function arrowRight(){
+        arro.style.marginLeft = "0px";
+        arro.textContent = "\u25B6";
       }
-    }
-    list +="</ul>"
-    return list;
-  }
+
+      function arrowLeft(){
+        arro.style.marginLeft = "-25px";
+        arro.textContent = "\u25C0";
+      }
+
+      function showPane(){
+        var i = 0, j = movers.length;
+        showing = 1;
+        
+        setTimeout(arrowRight,100)
+
+        if(oldIE){
+          for(;i<j;i++){
+            if(movers[i] === rp)
+              fx.animateProperty({node:movers[i], duration:300, properties:{marginRight:0}}).play();
+            else fx.animateProperty({node:movers[i], duration:300, properties:{marginRight:285}}).play();
+          }
+        }else{
+          for(;i<j;i++)
+            domClass.add(movers[i],"movd");
+        }
+      }
 
 
 
-  function makeSpaced(name){
-    return name.replace(/_/g," ")
-  }
+      function hidePane(){
+        var i = 0, j = movers.length;
+        showing = 0;
 
+        setTimeout(arrowLeft,100)
 
-
-  function getAttributeHTML(value){
-    var pboLink = /http:\/\/pboshared\.unavco\.org.*/i;
-    var regLink = /(?:^https?|^ftp):\/\//i;
-    var hydstraImg = /^<img.*hydstra/i;
-
-    if(pboLink.test(value))
-      return makeImage(value);
-    else if(regLink.test(value))
-      return '<a target="_blank" href="'+value+'">'+value+'</a>'
-    else if(hydstraImg.test(value))
-      return makeEmbedded(value,0);
-    else
-      return value;
-  }
-
-
-
-  function makeImage(link){
-    setTimeout(function(){
-      infoWindow.resize(560,420);
-      tabs.resize();
-    },0)
-    var image = '<div class="identifyLinkImage" style="background-image:url('+link+')"></div>';
-    return '<a target="_blank" href="'+link+'">'+image+'</a>'
-  }
-
-
-
-  function makeEmbedded(value){
-    var urlReg = /src=['"](.*?)['"]/;
-    var url = urlReg.exec(value)[1];
-
-    value = value.slice(0,5)+'style="width:512px;height:384px;" '+value.slice(5);
-
-    setTimeout(function(){
-      infoWindow.resize(612,500);
-      tabs.resize();
-    },0)
-
-    return '<a target="_blank" href="'+url+'">'+value+'</a>'
-  }
-
-
-
-  function getBlurb(title,isChange,url){
-    var type;
-    if(isChange){
-        return "<span>Groundwater change from "+ title+".</span>";
-    }else{
-      if(url.match("GIC_Elevation"))
-        type = "Groundwater elevation measured in "
-      else type = "Water depth below ground measured in "
-      return "<span>"+ type + name +".</span>"
-    }
-  }
-
-
-
-  function setNoData(){
-    var tab = new ContentPane({
-        content:"<p>No Data</p>",
-        title:"No Data"
-      })
-      tabs.addChild(tab);
-  }
-
-
-
-
-
-
-
-  closeToggle = function(){
-    var showing = 0;
-    var arro = dom.byId("arro");
-    var movers = query(".mov");
-
-    function arrowRight(){
-      arro.style.marginLeft = "0px";
-      arro.textContent = "\u25B6";
-    }
-
-    function arrowLeft(){
-      arro.style.marginLeft = "-25px";
-      arro.textContent = "\u25C0";
-    }
-
-    function showPane(){
-      var i = 0, j = movers.length;
-      showing = 1;
-      
-      setTimeout(arrowRight,100)
-
-      if(oldIE){
-        for(;i<j;i++){
+        if(oldIE){
+          for(;i<j;i++){
           if(movers[i] === rp)
-            fx.animateProperty({node:movers[i], duration:300, properties:{marginRight:0}}).play();
-          else fx.animateProperty({node:movers[i], duration:300, properties:{marginRight:285}}).play();
+            fx.animateProperty({node:movers[i], duration:250, properties:{marginRight:-285}}).play();
+          else fx.animateProperty({node:movers[i], duration:250, properties:{marginRight:0}}).play();
+          }
+        }else{
+          for(;i<j;i++)
+            domClass.remove(movers[i],"movd");
         }
-      }else{
-        for(;i<j;i++)
-          domClass.add(movers[i],"movd");
       }
-    }
 
-
-
-    function hidePane(){
-      var i = 0, j = movers.length;
-      showing = 0;
-
-      setTimeout(arrowLeft,100)
-
-      if(oldIE){
-        for(;i<j;i++){
-        if(movers[i] === rp)
-          fx.animateProperty({node:movers[i], duration:250, properties:{marginRight:-285}}).play();
-        else fx.animateProperty({node:movers[i], duration:250, properties:{marginRight:0}}).play();
-        }
-      }else{
-        for(;i<j;i++)
-          domClass.remove(movers[i],"movd");
+      return function(){
+        if(showing) hidePane();
+        else showPane();
       }
+    }();
+
+
+
+
+
+    //Show the application
+    function resetDataHeight (){
+      dataNode.style.height = DOC.documentElement.offsetHeight - 134 + "px"
     }
 
-    return function(){
-      if(showing) hidePane();
-      else showPane();
-    }
-  }();
 
-
-
-  on(closeButton,"mousedown", closeToggle);
-  dom.byId("mainContainer").style.visibility="visible";
-  W.setTimeout(function(){
-    on.emit(closeButton, "mousedown",{bubbles:true,cancelable:true})
-  },300);
-
-
-
-
-  function populateRightPane(title,data){
-    titleNode.innerHTML = title;
-    layerNode.innerHTML = data;
     resetDataHeight();
-  }
+    on(W,"resize",resetDataHeight)
+    on(closeButton,"mousedown", closeToggle);
+    dom.byId("mainContainer").style.visibility="visible";
+
+    W.setTimeout(function(){
+      on.emit(closeButton, "mousedown",{bubbles:true,cancelable:true})
+    },300);
 
 
 
+    //This needs to be repurposed to pull in the download module.. then step through
+    //visible layers and ask them to provide download links, then hand these in a flattened array
+    //to the download module
+    on(dom.byId("downloadLink"),"click",downloadZips)
+    function downloadZips(){
+      makeDownloads(getDataZips())
+    }
+
+    //Provide to the layers to add to the right pane
+    function populateRightPane(title,data){
+      titleNode.innerHTML = title;
+      dataNode.innerHTML = data;
+      resetDataHeight();
+    }
 
 
-  function resetDataHeight (){
-    layerNode.style.height = DOC.documentElement.offsetHeight - 134 + "px"
-  }
-
-  resetDataHeight();
-  on(W,"resize",resetDataHeight)
-
-
-
-
-
-  //This needs to be repurposed to pull in the download module.. then step through
-  //visible layers and ask them to provide download links, then hand these in a flattened array
-  //to the download module
-  on(dom.byId("downloadLink"),"click",downloadZips)
-  function downloadZips(){
-    makeDownloads(getDataZips())
-  }
-
+    //ie shim
+    function forEach(arr,fn){
+      for(var i=0;i<arr.length;i++){
+        fn(arr[i],i,arr)
+      }
+    }
 
 
 
