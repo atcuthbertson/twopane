d8ce3b14f4e71c7cc4571e202207b873ea366b1d
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-09-17 11:03:51 -0700

    removed spinner from main js. Need to develop into module.

--- js/twopane.js
+++ js/twopane.js
@@ -227,49 +227,6 @@ function(
 
 
 
-
-    //could/should likely be a module that each of the layers add
-    function addLoading(check){
-      var img = DOC.createElement('img')
-      img.className = "loadingImg";
-      img.src = "images/loading.gif";
-      check.parentNode.insertBefore(img,check)
-    }
-
-    function removeLoading(check){
-      var p = check.parentNode;
-      var prev = check.previousSibling
-      if(prev&&prev.tagName === 'IMG')
-        p.removeChild(prev)
-    }
-
-    function toggleLoading(check){
-      if(check.checked) removeLoading(check);
-    }
-
-    function layerUpdateEnd(e){
-      var layer = e.target;
-      if(layer){
-        toggleLoading(getCheckFromLayer(layer))
-      }
-    }
-
-    function getCheckFromLayer(layer){
-      var arr = layer.url.split('_');
-      var type = arr[arr.length-1].split('/')[0];
-      switch(type){
-        case "Points":
-          return measurementCheck;
-        case "Contours":
-          return contoursCheck;
-        case "Ramp":
-          return rampCheck;
-      }
-    }
-
-
-
-
     // Add dijits to the application
 
 
