03ff74c64e0fd27369d97e87aa3d11b5813c30b1
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-10-21 16:00:06 -0700

    Contained outrageous form submit retries. Very peculiar behavior.. it would try to walk to deeper directories and rerequest the file.. seems like a security issue and is certainly surprising default behavior

--- modules/downloadMultiple.js
+++ modules/downloadMultiple.js
@@ -3,13 +3,15 @@ define([],function(){
   var DOC = document;
 
   /* Iframe hack to download multiple files at once without server-side code */
-  /* This will throw errors quite spectacularly if the zip files don't exist */
+  /* This will throw a 404 if the file doesn't exist */
   function makeDownload(url){
     var ifr = DOC.createElement('iframe');
     ifr.style.display="none";
-
+    var first = 1;
 
     ifr.onload=function(){
+      if(!first) return;
+      first = 0;
       var ifrDoc = ifr.contentWindow||ifr.contentDocument;
       if(ifrDoc.document) ifrDoc = ifrDoc.document;
 
