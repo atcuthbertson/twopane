7aa5479d3e234b884ba80b76f94b6ff264c2bfb7
Wyatt Pearsall <wyattttttt@gmail.com>
2014-11-14 15:31:38 -0800

    Proper styling

--- modules/layers/radio.js
+++ modules/layers/radio.js
@@ -16,24 +16,39 @@ define(["modules/layers/baseCheck.js"],function(baseCheck){
     
     var form = document.createElement('form'); 
     var container = document.createElement('div');
+    var dataType = document.createElement('h4');
+    var radioName = Math.random();
 
     form.className = 'radioForm';
-
+    dataType.textContent = dataType.innerText = "Select Data Type";
+    dataType.className = 'divisionHeader';
+    form.appendChild(dataType);
+     
     for(var i=0; i <urls.length; i++){   
       var serviceName = makeSpaced(urls[i].match(nameReg)[1]);
+      var wrap = document.createElement('div');
       var inp = document.createElement('input');
       var label = document.createElement('label');
       var inpId = Math.random();
 
       inp.id = inpId;
+      inp.className = 'radioInput';
       inp.type = 'radio';
+      inp.name = radioName;
+      if(i===0) inp.checked = "checked";
       label.setAttribute('for',inpId);
       label.textContent = label.innerText = serviceName;
       
-      form.appendChild(inp);
-      form.appendChild(label); 
+      wrap.appendChild(inp);
+      wrap.appendChild(label); 
+      form.appendChild(wrap);
     }     
     container.appendChild(form);
+
+    var showLayers = document.createElement('h4');
+    showLayers.className = 'divisionHeader';
+    showLayers.textContent = showLayers.innerText = 'Show Layers';
+    container.appendChild(showLayers);
 
     return baseCheck(urls[0], container, resolver, map, hookService, options);
   }
