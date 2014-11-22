692a51e1f499db49b30123914caf9a40088804a2
Wyatt Pearsall <wyattttttt@gmail.com>
2014-11-21 17:07:07 -0800

    Make services separated from UI

---
+++ modules/layers/makeServices.js
@@ -0,0 +1,118 @@
+define([
+  "dojo/on",
+
+  "esri/layers/ArcGISDynamicMapServiceLayer",
+
+  "modules/layerQueue.js",
+  "modules/info.js",
+  ],
+function(
+  on,
+
+  ArcGISDynamicMapServiceLayer,
+
+  layerQueue,
+  info,
+){
+
+  var nameReg = /([^\/]*)\/MapServer/;
+
+
+  function makeSpaced(name){
+    return name.replace(/_/g," ")
+  }
+
+  function makeUnderscored(name){
+    return name.replace(/ /g,"_")
+  }
+
+  function makeService(url){
+    var service = new ArcGISDynamicMapServiceLayer(url);
+    service.suspend();
+    return service;
+  }
+
+
+  return function(url, map, attachUI, options){
+
+
+    var exclude = options.exclude || [];
+    var downloader = options.downloader || null;
+    var excludeDownload = options.excludeDownload || [];
+
+
+    function processLayer(serviceObj){
+      var firstService = serviceObj.service;
+      var layer = serviceObj.evt.layer;
+      var services = [];
+
+      var url = layer.url
+      var layerInfos = layer.layerInfos;
+      var layerCount = layerInfos.length;
+
+      var serviceName = makeSpaced(url.match(nameReg)[1]);
+      var serviceUnderscored = makeUnderscored(serviceName);
+
+      info.register(url);
+      excludeDownloads(serviceUnderscored, layerInfos);
+     
+      /*One map layer for each service layer, for independent transparencies*/
+      //
+      //  Make all services, augment services, call passed function
+      //  This function will decide how many checks to build
+      //  and how to hook them to each service
+      //  (if params exist, for each param, build a mapping from
+      //  param name to service.. then carry this over to the check/resolver)
+      //
+      for(var i=0; i<layerCount; i++){
+        var service;
+        var underscoredName = makeUnderscored(layerInfos[i].name);
+        if(exclude.indexOf(underscoredName) !== -1) continue;
+        if(i>0) service = makeService(url);
+        else service = firstService;  
+  
+        service.setVisibleLayers([i]);
+        service.fullName =  serviceUnderscored + "/" + underscoredName;
+        service.service = serviceUnderscored; 
+        map.addLayer(service, 1);
+        services.push(service);
+      }
+      attachUI(services); 
+
+    }
+
+
+    function toggleLayer(service){
+      if(service.suspended){
+        service.resume();
+        info.activate(service);
+        if(downloader) downloader.add(service.fullName);
+      }else{
+        service.suspend();
+        info.deactivate(service);
+        if(downloader) downloader.remove(service.fullName);
+      }
+    }
+
+
+
+    function excludeDownloads(serviceUnderscored, layerInfos){
+      var i;
+      if(downloader && excludeDownload.length){
+        if(excludeDownload[0] === "*"){
+          for (i = 0; i < layerInfos.length; i++) {
+            excludeDownload[i] =  serviceUnderscored + "/" + makeUnderscored(layerInfos[i].name);
+          }
+        }else{
+          for(i = 0; i<excludeDownload.length; i++){
+            excludeDownload[i] = serviceUnderscored + "/" + makeUnderscored(excludeDownload[i]);
+          }
+        }
+        downloader.exclude(excludeDownload);
+      }
+    }
+
+
+
+  }
+});
