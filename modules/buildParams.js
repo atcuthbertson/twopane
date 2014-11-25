f6d1f95c5e64f9214694f6b65fedf3af6bb52af7
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-11-25 11:25:06 -0800

    Service grouping.. may need to pass a resolving fn to buildParams

--- modules/buildParams.js
+++ modules/buildParams.js
@@ -1,6 +1,28 @@
 define([],function(){
 
-  return function(keyLayers, services, resolver, container, options){
+  function groupServices(services, keyLayers){
+    var groups = {};
 
+    for(var h = 0; h<keyLayers.length; h++){
+      groups[keyLayers[h]] = [];
+    }
+
+    for(var i=0; i<services.length; i++){
+      for(var j=0; j<keyLayers.length; j++){
+        var service = services[i];
+        var layer = keyLayers[j];
+        if(service.layerName.indexOf(layer) > -1){
+          groups[layer].push(service);
+          break;
+        }
+      }
+    }
+
+    return groups;
+  }
+
+
+  return function(services, keyLayers, resolver, container, options){
+    var serviceGroups = groupServices(keyLayers, services)
   }
 });
