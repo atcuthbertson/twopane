7d19b787522a8443908d0d7fac02723810ebf4b0
Wyatt Pearsall <wyattttttt@gmail.com>
2014-12-30 13:05:46 -0800

    Trim util

--- modules/utils.js
+++ modules/utils.js
@@ -1,6 +1,6 @@
 define([],function(){
   var nameReg = /([^\/]*)\/MapServer/;
-
+  var trimStr = /^[_\s]+|[_\s]+$/g; 
   function makeSpaced(name){
     return name.replace(/_/g," ")
   }
@@ -13,8 +13,14 @@ define([],function(){
     return url.match(nameReg)[1])
   }
 
+  function trim(str){
+    return str.replace(trimStr,'')
+  }
+
   return {
     space:makeSpaced,
-    underscore:makeUnderscored
+    underscore:makeUnderscored,
+    getServiceName:getServiceName,
+    trim:trim
   }
 });
